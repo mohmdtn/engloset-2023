18cc6a386a85ea82f732b3c432c19b1b50f8f9fb
mohammad taghinasab <mohammad.sawdegh@gmail.com>
2023-06-09 10:36:03 +0330

    add function to fill bullets and ccontrol active classes

--- 02 02 08 introduction popups/js/script.js
+++ 02 02 08 introduction popups/js/script.js
@@ -1,33 +1,36 @@
 $(document).ready(function () {
     var swiper = new Swiper('.mySwiper', {
         spaceBetween: 20,
+        allowTouchMove: false,
         pagination: {
             el: ".swiper-pagination",
-            clickable: true,
         },
         navigation: {
             nextEl: '.next-btn',
         },
     });
 
-    if ($(".green").hasClass('deactive')) {
-        $(".green-slide").remove();
-    }
-    if ($(".blue").hasClass('deactive')) {
-        $(".blue-slide").remove();
-    }
-    if ($(".orange").hasClass('deactive')) {
-        $(".orange-slide").remove();
-    }
-    if ($(".purple").hasClass('deactive')) {
-        $(".purple-slide").remove();
-    }
-    if ($(".pink").hasClass('deactive')) {
-        $(".pink-slide").remove();
-    }
+    $(".next-btn").click(function (e) { 
+        $('.swiper-pagination-bullet-active').addClass('.bullet-fill').removeClass('.swiper-pagination-bullet-active');
+        $('.swiper-pagination-bullet-active').prevAll().addClass('bullet-fill');
+
+        getCompleteIndex = $('.swiper-pagination-bullet-active').index();
+        if(getCompleteIndex == 2){
+            $(".introduction-popups .images .orange").removeClass("deactive");
+        }
+        if(getCompleteIndex == 3){
+            $(".introduction-popups .images .blue").removeClass("deactive");
+        }
+        if(getCompleteIndex == 5){
+            $(".introduction-popups .images .purple").removeClass("deactive");
+        }
+        if(getCompleteIndex == 7){
+            $(".introduction-popups .images .pink").removeClass("deactive");
+        }
+    });
+
 
     $(".image").click(function (e) {
-        console.log(e);
         if (!$(this).hasClass('deactive')) {
             let index = $(this).data("index");
             swiper.slideTo(index);
